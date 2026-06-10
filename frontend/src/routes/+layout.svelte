0bca386f8f217894c10920156057f7d5e60df7b9
SunnyCloudYang <sunnycloudyang@outlook.com>
2026-06-10 21:51:22 +0800

    style: add design system tokens and base layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+</svelte:head>
+
+{@render children()}
